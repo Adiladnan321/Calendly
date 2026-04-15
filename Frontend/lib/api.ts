d55766e9a52d6d9c424068c882b124f6b541ffca
Adiladnan321 <adiladnan321@gmail.com>
2026-04-16 00:20:21 +0530

    added ec2

--- Frontend/lib/api.ts
+++ Frontend/lib/api.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") ?? "http://localhost:4000";
+  process.env.NEXT_PUBLIC_API_URL?? "http://localhost:4000";
 
 async function request<T>(path: string, init?: RequestInit): Promise<T> {
   const response = await fetch(`${API_BASE_URL}${path}`, {
